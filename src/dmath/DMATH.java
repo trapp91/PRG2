c7e0333874730476fe1ddf4d339f1cbb16b6a2aa
Simon <Simon@Laptop-ASUSN76V>
2013-10-06 21:20:29 +0200

    Init

---
+++ src/dmath/DMATH.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmath;
+
+/**
+ *
+ * @author Simon
+ */
+public class DMATH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
